d86625ea4a535d852c8a4529db297f932dd83017
doron lisbona <lisbona107@gmail.com>
2018-12-11 23:10:17 +0200

    missing form php

---
+++ clashes-master/php/contact.php
@@ -0,0 +1,72 @@
+<?php 
+
+  require 'autoload.php'
+  if(isset($_POST['email'])){
+	  
+	$name =$_POST["name"];
+	$phone =$_POST["phoneNumber"];
+	$from =$_POST["email"];
+	$date=$_POST["date"];
+	$hour=$_POST["hour"];
+	
+	// Email Receiver Address
+	$receiver="lisbona107@gmail.com";
+	$receiver2="clash.israel9@gmail.com";
+	$subject="message from customer";
+
+	$message = "
+	<html>
+	<head>
+	<title>HTML email</title>
+	</head>
+	<body>
+	<table width='50%' border='0' align='center' cellpadding='0' cellspacing='0'>
+	<tr>
+	<td colspan='2' align='center' valign='top'><img style=' margin-top: 15px; ' src='../images/logo-black.png' ></td>
+	</tr>
+	<tr>
+	<td width='50%' align='right'>&nbsp;</td>
+	<td align='left'>&nbsp;</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>שם:</td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$name."</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>אימייל:</td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$from."</td>
+    </tr>
+    <tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>טלפון:</td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$phone."</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>תאריך:</td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$date."</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; border-bottom:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>שעה:</td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; border-bottom:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".nl2br($hour)."</td>
+	</tr>
+	</table>
+	</body>
+	</html>
+	";
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+// More headers
+$headers .= 'From: <'.$from.'>' . "\r\n";
+   if(mail($receiver,$subject,$message))  
+   {
+	   //Success Message
+      echo "ההודעה נשלחה בהצלחה, נחזור אליך עד יום עסקים אחד";
+      mail($receiver2,$subject,$message)
+   }
+   else
+   {	
+   	 //Fail Message
+      echo "ההודעה אינה נשלחה, נסה שוב!";
+   }
+}
+?>
